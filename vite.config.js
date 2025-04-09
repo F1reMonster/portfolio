1f66a4914d920538fb21a34c4a21fc7a6b8a097a
F1reMonster <f1remonster@proton.me>
2025-04-09 22:44:01 +0300

    remove commet

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import path from "path";
-import fs from "fs";
-import { viteStaticCopy } from "vite-plugin-static-copy";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+import fs from 'fs';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 function getHtmlEntryFiles(srcDir) {
 	const entry = {};
@@ -11,16 +11,16 @@ function getHtmlEntryFiles(srcDir) {
 	function traverseDir(currentDir) {
 		const files = fs.readdirSync(currentDir);
 
-		files.forEach((file) => {
+		files.forEach(file => {
 			const filePath = path.join(currentDir, file);
 			const isDirectory = fs.statSync(filePath).isDirectory();
 
 			if (isDirectory) {
 				// If it's a directory, recursively traverse it
 				traverseDir(filePath);
-			} else if (path.extname(file) === ".html") {
+			} else if (path.extname(file) === '.html') {
 				// If it's an HTML file, add it to the entry object
-				const name = path.relative(srcDir, filePath).replace(/\..*$/, "");
+				const name = path.relative(srcDir, filePath).replace(/\..*$/, '');
 				entry[name] = filePath;
 			}
 		});
@@ -31,25 +31,21 @@ function getHtmlEntryFiles(srcDir) {
 	return entry;
 }
 
-// https://vite.dev/config/
 export default defineConfig({
-	root: "src",
-	base: "./",
+	root: 'src',
+	base: './',
 	build: {
 		rollupOptions: {
-			input: getHtmlEntryFiles("src"),
+			input: getHtmlEntryFiles('src'),
 			output: {
-				assetFileNames: (assetInfo) => {
+				assetFileNames: assetInfo => {
 					if (/\.css$/.test(assetInfo.name)) {
 						return `_assets/css/[name]-[hash].css`;
 					}
 					if (/\.png$|\.jpe?g$|\.svg$|\.gif$|\.webp$/.test(assetInfo.name)) {
-						return "_assets/img/[name][extname]"; // Без хешу
+						return '_assets/img/[name][extname]'; // Без хешу
 					}
 
-					// if (/\.js$/.test(assetInfo.name)) {
-					// 	return `assets/js/[name][extname]`;
-					// }
 					if (/\.json$/.test(assetInfo.name)) {
 						return `_assets/data/[name][extname]`;
 					}
@@ -57,14 +53,14 @@ export default defineConfig({
 					if (/woff|woff2/.test(assetInfo.name)) {
 						return `_assets/fonts/[name][extname]`;
 					}
-					return "_assets/[name][extname]";
+					return '_assets/[name][extname]';
 				},
 
 				entryFileNames: `_assets/js/[name]-[hash].js`,
 				chunkFileNames: `_assets/js/[name]-[hash].js`,
 			},
 		},
-		outDir: "../dist",
+		outDir: '../dist',
 		emptyOutDir: true,
 	},
 	server: {
@@ -76,10 +72,7 @@ export default defineConfig({
 		react(),
 		tailwindcss(),
 		viteStaticCopy({
-			targets: [
-				
-				{ src: "data", dest: "./" },
-			],
+			targets: [{ src: 'data', dest: './' }],
 		}),
 	],
 });
